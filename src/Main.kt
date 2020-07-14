cfd2adeb97d5782590dc3dfd64761e29c9f21c8b
Yahya <yahyatinani@gmail.com>
2020-07-14 14:05:06 +0100

    Abstract the co-recursive behavior to be reusable

--- src/Main.kt
+++ src/Main.kt
@@ -5,13 +5,13 @@ fun preAppend(c: Char, s: String): String = "$c $s"
 fun <T> rest(collection: List<T>): List<T> = collection.drop(1)
 
 fun toString(list: List<Char>): String =
-    if (list.isEmpty()) ""
-    else preAppend(list[0], toString(list.subList(1, list.size)))
+        if (list.isEmpty()) ""
+        else preAppend(list[0], toString(list.subList(1, list.size)))
 
 fun toStringTail(list: List<Char>): String {
     tailrec fun toStringTailIter(list: List<Char>, s: String): String =
-        if (list.isEmpty()) s
-        else toStringTailIter(rest(list), append(s, list.first()))
+            if (list.isEmpty()) s
+            else toStringTailIter(rest(list), append(s, list.first()))
 
     return toStringTailIter(list, "")
 }
@@ -20,7 +20,7 @@ fun sum(n: Int): Int = if (n == 0) 0 else n + sum(n - 1)
 
 fun sumTail(n: Int): Int {
     tailrec fun sumTailIter(n: Int, s: Int): Int =
-        if (n == 0) s else sumTailIter(n - 1, s + n)
+            if (n == 0) s else sumTailIter(n - 1, s + n)
 
     return sumTailIter(n, 0)
 }
@@ -36,20 +36,46 @@ tailrec fun add(a: Int, b: Int): Int {
 }
 
 fun <T> makeString(list: List<T>, delim: String): String =
-    when {
-        list.isEmpty() -> ""
-        list.drop(1).isEmpty() -> "${list.first()} ${makeString(list.drop(1), delim)}"
-        else -> "${list.first()} $delim ${makeString(list.drop(1), delim)}"
-    }
+        when {
+            list.isEmpty() -> ""
+            list.drop(1).isEmpty() -> "${list.first()} ${makeString(list.drop(1), delim)}"
+            else -> "${list.first()} $delim ${makeString(list.drop(1), delim)}"
+        }
 
 fun <T> makeStringTail(list: List<T>, delim: String): String {
 
     tailrec fun makeStringTailIter(list: List<T>, acc: String): String =
-        when {
-            list.isEmpty() -> acc
-            acc.isEmpty() -> makeStringTailIter(list.drop(1), "${list.first()}")
-            else -> makeStringTailIter(list.drop(1), "$acc $delim ${list.first()}")
-        }
+            when {
+                list.isEmpty() -> acc
+                acc.isEmpty() -> makeStringTailIter(list.drop(1), "${list.first()}")
+                else -> makeStringTailIter(list.drop(1), "$acc $delim ${list.first()}")
+            }
 
     return makeStringTailIter(list, "")
+}
+
+fun <T, U> foldLeft(list: List<T>, acc: U, f: (T, U) -> U): U {
+    tailrec fun foldLeftIter(list: List<T>, acc: U): U =
+            if (list.isEmpty()) acc
+            else foldLeftIter(list.drop(1), f(list.first(), acc))
+
+    return foldLeftIter(list, acc)
+}
+
+fun sumFoldLeft(n: Int): Int = foldLeft((0..n).toList(), 0, Int::plus)
+
+fun toStringFoldLeft(list: List<Char>): String =
+        foldLeft(list, "", { c, acc -> append(acc, c) })
+
+fun <T> f(delim: String): (T, String) -> String =
+        { item: T, acc: String ->
+            if (acc.isEmpty()) "$acc $item"
+            else "$acc $delim $item"
+        }
+
+fun <T> makeStringFoldLeft(list: List<T>, delim: String): String {
+    return foldLeft(list, "", { item: T, acc: String ->
+        if (acc.isEmpty()) "$item"
+        else "$acc $delim $item"
+    })
 }
