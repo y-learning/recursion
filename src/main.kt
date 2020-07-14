b886733bd151d45fa4d5b8863c21b1ce3668deb9
Yahya <yahyatinani@gmail.com>
2020-07-14 09:50:48 +0100

    Practice some recursion & co-recursion

---
+++ src/main.kt
@@ -0,0 +1,60 @@
+import java.math.BigInteger
+
+fun append(s: String, c: Char): String = "$s $c"
+
+fun preAppend(c: Char, s: String): String = "$c $s"
+
+fun <T> rest(collection: List<T>): List<T> = collection.drop(1)
+
+fun toStringCoRecursion(list: List<Char>): String {
+    tailrec fun toString(list: List<Char>, s: String): String {
+        if (list.isEmpty()) return s
+
+        return toString(rest(list), append(s, list.first()))
+    }
+
+    return toString(list, "")
+}
+
+fun toStringRecursion(list: List<Char>): String =
+    if (list.isEmpty()) ""
+    else preAppend(list[0], toStringRecursion(list.subList(1, list.size)))
+
+fun sum(n: Int): Int = if (n == 0) 0 else n + sum(n - 1)
+
+fun sumCo(n: Int): Int {
+    tailrec fun sumCo(n: Int, s: Int): Int =
+        if (n == 0) s else sumCo(n - 1, s + n)
+
+    return sumCo(n, 0)
+}
+
+fun inc(n: Int) = n + 1
+
+fun dec(n: Int) = n - 1
+
+tailrec fun add(a: Int, b: Int): Int {
+    if (a == 0) return b
+
+    return add(dec(a), inc(b))
+}
+
+val factorial: (Int) -> Int by lazy {
+    { n: Int ->
+        if (n == 1) n
+        else n * factorial(n - 1)
+    }
+}
+
+object Factorial {
+    val factorial: (Int) -> Int by lazy {
+        { n: Int ->
+            if (n == 1) n
+            else n * factorial(n - 1)
+        }
+    }
+}
+
+fun main() {
+
+}
